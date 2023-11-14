68656c26e539880eb0995eb89234b6c8a3890548
Breno <brenogiovanniolintho@gmail.com>
2023-11-14 17:56:43 -0300

    Auth UI

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -9,13 +9,15 @@ import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, For
 import { Input } from "@/components/ui/input"
 
 import { useCallback, useState } from "react"
+import AuthSocialButton from "./AuthSocialButton"
+import {BsGithub, BsGoogle} from 'react-icons/bs'
 
 type Variant = 'LOGIN' | 'REGISTER'
 
 const formSchema = z.object({
     name: z.string().min(2, {message: "Username must be at least 2 characters.",}),
     email: z.string().email(),
-    password: z.string().min(8)
+    password: z.string().min(8, {message: "Password must be at least 8 characters."})
   })
 
 export default function AuthForm() {
@@ -30,7 +32,7 @@ export default function AuthForm() {
         }
     }, [variant])
 
-    const {register, handleSubmit, formState:{errors}} = useForm<z.infer<typeof formSchema>>({
+    const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
             name: '',
@@ -58,8 +60,69 @@ export default function AuthForm() {
     }
 
     return (
-        <section>
-            AuthForm2
+        <section className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+            <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+                    {variant === 'REGISTER' && (
+                        <FormField control={form.control} name="name" render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Name</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="e.g Mark Cillessen" {...field} />
+                                </FormControl>
+                                <FormDescription>Enter your name. It will be displayed publicly.</FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                            )} />
+                    )}
+
+                        <FormField control={form.control} name="email" render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email Address</FormLabel>
+                            <FormControl>
+                                <Input placeholder="" {...field} />
+                            </FormControl>
+                            <FormDescription>Type your e-mail</FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                        )} />
+                        <FormField control={form.control} name="password" render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input placeholder="" {...field} />
+                            </FormControl>
+                            <FormDescription>Type your password</FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                        )} />
+                    <Button className="w-full" disabled={isLoading} variant='blue' type="submit">{variant === 'LOGIN' ? 'Sign in' : 'Register'}</Button>
+                </form>
+            </Form>
+            <div className="mt-6">
+                <div className="relative">
+                    <div className="absolute inset-0 flex items-center">
+                        <div className="w-full border-t border-gray-300" />
+                    </div>
+                    <div className="relative flex justify-center text-sm">
+                        <span className="bg-white px-2 text-gray-500">Or continue with</span>
+                    </div>
+                </div>
+                <div className="mt-6 flex gap-2 justify-center">
+                    <AuthSocialButton icon={BsGithub} onClick={() => socialAction('github')} />
+                    <AuthSocialButton icon={BsGoogle} onClick={() => socialAction('google')} />
+                </div>
+            </div>
+            <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+                <div>
+                    {variant === 'LOGIN' ? 'New to Messenger?' : 'Already have an account?'}
+                </div>
+                <div onClick={toggleVariant} className="underline cursor-pointer">
+                    {variant === 'LOGIN' ? 'Create an Account' : 'Login'}
+                </div>
+            </div>
+            </div>
         </section>
     )
 }
